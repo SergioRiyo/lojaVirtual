a9c7ea1241068598c1f74f4f7521cae7d41590e2
Riyo <riyooshiropao@gmail.com>
2025-05-22 23:23:25 -0400

    ativ12

---
+++ src/routers/Routers.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CadastroLogin from "../pages/cadastroLogin";
+import {Routes,Route} from "react-router-dom";
+import Login from "../pages/login";
+import Produto from "../conponentes/produto";
+export default function Routers() {
+  return (
+    <Routes>
+      <Route index element={<Produto />} />
+      <Route path="/cadastroLogin" element={<CadastroLogin/>} />
+      <Route path="/produtos" element={<Produto/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+  );
+}
